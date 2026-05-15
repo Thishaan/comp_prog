af8007aaa341cb5e1349b16bf662f9b357d2436b
Thishaan <udia@wisc.edu>
2026-05-14 23:29:04 -0500

    absolutely diabolical performance

---
+++ prac_comp456/C_not_adjcent.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+
+
+using namespace std;
+using set =   unordered_set<char>; 
+
+int main(){
+
+  string S;
+  
+  cin >> S;
+  size str_end = S.length();
+  size_t len_curr = S.length();
+  int start, end;
+  start = 0;
+  end  = 1;
+  
+  set my_set;
+  
+  my_set.insert(S[start]);
+  my_set.insert(S[end]);
+  
+  while(end != str_len || start !=str_len){
+        
+        if(end != str_len ){
+          
+        
+        }else if(end != str_len){
+          
+        }
+  }
+  
+  
+  return 0;
+}
