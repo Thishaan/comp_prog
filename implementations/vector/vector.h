7c30d910e3d437c7d63f6d79254167175158d965
Thishaan <udia@wisc.edu>
2026-05-30 14:57:52 -0500

    addig vector implementatins now

---
+++ implementations/vector/vector.h
@@ -0,0 +1,63 @@
+#include <cstdlib>
+
+template<typename T>
+
+class vector{
+    typedef struct itearator{
+        T * iterator; 
+    }iter;
+
+    private:
+
+    T * arr; // underlying array
+    size_t size;
+    size_t capacity;
+
+    public:
+
+   
+
+    inline vector(size_t size=0, T val = 0){
+
+        this->size =  size;
+
+        if(size == 0 ){
+             arr= nullptr;
+             capacity=0;
+        }
+
+        else{
+
+            arr = new T(size);// allocat the array;
+            for(size_t i =0; i < size; i++){
+                arr =  val;
+            }
+        }
+    }
+
+    void push_back();
+    void swap();
+    bool operator ==(vector & other);
+    vector opreator= (const vector & other);
+    vector opreator= (vector & other);
+
+
+    T at(size_t index);
+    T front();
+    T back();
+    T operator[](size_t index); 
+
+
+    iter begin();
+    iter end(); 
+
+  //size_t end();
+
+    void pop_back();
+
+
+
+
+
+
+};
