2ed589d982a9a6e95408b86fa16feabc38e93a6a
Thishaan <udia@wisc.edu>
2026-06-20 17:04:02 -0500

    C failed

---
+++ comp_463/problem_C.cpp
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+using ll  = long long;
+using pair = Pair<ll ,ll>; 
+using vec =  vetor<pair>;
+
+
+
+int main(){
+  int N,Q;
+  pair p; 
+  vec pairs;
+  
+  
+  for(int i{}; i < N; ++i){
+    cin >> p.first >> p.second;
+  }
+  cin >> Q;
+  
+  vector<ll> T;
+   for(int i{}; i < Q; ++i){
+    ll Ti;
+    cin >> Ti;
+    T.push_back(Ti);
+  }
+    //**
+ // std::sort(vec.begin(), vec.end(), [](const auto& a, const auto& b) {
+  //  return a.second < b.second;
+//});
+
+  
+  
+  
+  for(int i{}; i < Q; ++i){
+    
+  }
+  
+  
+  return 0;
+}
