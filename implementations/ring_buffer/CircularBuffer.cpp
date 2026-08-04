c7fc7fc86b0a4018f94d4bebaa8bdbefe4e9cc49
Thishaan <udia@wisc.edu>
2026-08-05 00:35:15 +0100

    modified a bit

--- implementations/ring_buffer/CircularBuffer.cpp
+++ implementations/ring_buffer/CircularBuffer.cpp
@@ -1,5 +1,7 @@
 #include <exception>
 #include <iostream>
+#include <optional>
+#include <cstring>
 
 template <typename T>
 class CirculBuffer{
@@ -7,23 +9,40 @@ class CirculBuffer{
 
 
     private:
-        size_t size;
+        size_t capacity;
         size_t l;
         size_t r; // left and right pointers  
         T* arr; // underlying array;
 
     public:
 
-     explicit CirculBuffer(size_t cap): capacity{capacity}, l{}, r{}, size{} {
+     explicit CirculBuffer(size_t cap): capacity{cap}, l{}, r{}{
 
-        arr = new(T)[cap];   // allocate on heap 
+     CirculBuffer operator=(CirculBuffer & other){
+        
+        
+        other.capaicty = capacity;
+        other.r = r;
+        other.l = l;
+
+        // handle allocation separately
+        other.arr = new T[capacity];
+
+        std::memcpy(arr,other.arr, sizeof(T)*capacity)
+
+
+     }
+        if(cap < 1){
+            throw  std::out_of_range("invalid capacity, D NOT COSTRUCT");
+        }
+        arr = new T[cap];   // allocate on heap 
      }
     
 
-     void push( T & elem){
+     void push(T elem){
 
-        if(capacity == ((r-l)+1)){
-            throw std::out_of_range("size curently equals capacity");
+        if(capacity == (r-l)){
+         //  throw std::out_of_range("size curently equals capacity");
         }
         arr[r]  = T;
         r = (r+1)%capacity;
@@ -31,12 +50,15 @@ class CirculBuffer{
      }
 
 
-     void pop(){
+std::optional<T> pop(){
 
-        if(((r-l)+1) ==0){
-            throw std::out_of_range("size curently 0, cannot pop!");
+        if((r-l) ==0){
+           // throw std::out_of_range("size curently 0, cannot pop!");
+           return std::nullopt;  // didnt knw ts existed
         }
+        T item_ret = arr[l];// brev
         l = (l+1)%capacity;
+        return item_ret;
      }
 
 
